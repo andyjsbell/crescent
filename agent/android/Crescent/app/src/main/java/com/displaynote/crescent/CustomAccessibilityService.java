d0b5edeae56471a1390d894f6358e1ccb7aa3902
Andy Bell <andy.bell@displaynote.com>
2020-11-06 16:09:45 +0100

    accessibility

--- agent/android/Crescent/app/src/main/java/com/displaynote/crescent/CustomAccessibilityService.java
+++ agent/android/Crescent/app/src/main/java/com/displaynote/crescent/CustomAccessibilityService.java
@@ -36,7 +36,7 @@ public class CustomAccessibilityService extends AccessibilityService {
 
     //Respond to AccessibilityEvents
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
+    publxic void onAccessibilityEvent(AccessibilityEvent event) {
 
         if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED) {
             Toast.makeText(getApplication(), event.getText().toString(), Toast.LENGTH_SHORT).show();
