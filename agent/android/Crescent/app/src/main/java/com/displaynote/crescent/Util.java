c589f3702ca5cd962ef8c9cb77188dee5af01ccb
Andy Bell <39520002+andyjsbell@users.noreply.github.com>
2020-11-06 20:50:53 +0100

    Load file from assets into a string

--- agent/android/Crescent/app/src/main/java/com/displaynote/crescent/Util.java
+++ agent/android/Crescent/app/src/main/java/com/displaynote/crescent/Util.java
@@ -5,6 +5,10 @@ import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Util {
     public static void startJob(Context context) {
         JobScheduler jobScheduler = (JobScheduler)context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
@@ -15,5 +19,33 @@ public class Util {
                 .build();
 
         jobScheduler.schedule(jobInfo);
+    }
+
+    public static String readTextFileFromAssets(Context context, String filename) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(context.getAssets().open(filename), "UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            // do reading, usually loop until end of file reading
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                sb.append(mLine).append("\n");
+            }
+
+            return sb.toString();
+
+        } catch (IOException e) {
+            //log the exception
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+            return null;
+        }
     }
 }
