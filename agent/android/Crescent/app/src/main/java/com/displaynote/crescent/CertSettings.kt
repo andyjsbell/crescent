2f8cd699dcd040703b8e22e2dbc7d9dccef38bbc
Andy Bell <39520002+andyjsbell@users.noreply.github.com>
2020-11-10 13:48:08 +0100

    Allow file to be null

--- agent/android/Crescent/app/src/main/java/com/displaynote/crescent/CertSettings.kt
+++ agent/android/Crescent/app/src/main/java/com/displaynote/crescent/CertSettings.kt
@@ -2,5 +2,5 @@ package com.displaynote.crescent
 
 import java.io.File
 
-data class CertSettings(val secureCert: String?, val rootCert: String?, val claimCert: String?, val claimKey: String?, val endpoint: String?, val template: String?, val certPath: File) {
+data class CertSettings(val secureCert: String?, val rootCert: String?, val claimCert: String?, val claimKey: String?, val endpoint: String?, val template: String?, val certPath: File?) {
 }
