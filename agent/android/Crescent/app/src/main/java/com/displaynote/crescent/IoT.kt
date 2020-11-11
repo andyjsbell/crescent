38ff6dc93dd887e88c29913d5cd8f408afd4390f
Andy Bell <andy.bell@displaynote.com>
2020-11-11 13:17:09 +0100

    Pull out into IoT class

---
+++ agent/android/Crescent/app/src/main/java/com/displaynote/crescent/IoT.kt
@@ -0,0 +1,109 @@
+package com.displaynote.crescent
+
+import android.content.Context
+import android.hardware.usb.UsbEndpoint
+import android.util.Log
+import java.io.File
+import java.io.FileNotFoundException
+import java.util.*
+
+class IoT(
+        private val context: Context,
+        private val certPath: File?,
+        private val endpoint: String?,
+        private val provisioningTemplate: String?
+) {
+    private lateinit var rootCert : String
+    private lateinit var certName : String
+    private lateinit var keyName : String
+    private lateinit var claimCert : String
+    private lateinit var claimKey : String
+    private var client : Client? = null
+
+    init {
+        Log.d(MainActivity.TAG, "Initialising IoT subsystem")
+        loadCerts()
+        provisionDevice()
+        if (certName.isNotEmpty() && keyName.isNotEmpty()) {
+            val cert = File(certPath, certName).readText()
+            val key = File(certPath, keyName).readText()
+            val clientSettings = ClientSettings(getClientId(certPath), cert, key, rootCert, endpoint.toString())
+            client = Client(clientSettings)
+            client?.connect()
+            Log.d(MainActivity.TAG, "Connected to IoT")
+        }
+    }
+
+    private fun loadCerts() {
+        val applicationContext = context.applicationContext
+        rootCert = Util.readTextFileFromAssets(
+                context,
+                "AmazonRootCA1.pem").toString()
+
+        certPath?.walkTopDown()?.filter { file ->
+            file.isFile && file.extension == "crt"
+        }?.forEach { certName = it.name }
+
+        certPath?.walkTopDown()?.filter { file ->
+            file.isFile && file.extension == "key"
+        }?.forEach { keyName = it.name }
+
+        claimCert = Util.readTextFileFromAssets(
+                applicationContext,
+                context.getString(R.string.claimCert)
+        ).toString()
+
+        claimKey = Util.readTextFileFromAssets(
+                applicationContext,
+                context.getString(R.string.claimKey)
+        ).toString()
+    }
+
+    private fun provisionDevice() {
+        if (certName.isEmpty() || keyName.isEmpty()) {
+            val settings = certPath?.let {
+                ProvisionSettings(
+                        getClientId(certPath),
+                        claimCert,
+                        claimKey,
+                        rootCert,
+                        endpoint.toString(),
+                        provisioningTemplate.toString(),
+                        it
+                )
+            }
+            val provisionClient = settings?.let { ProvisioningClient(it) }
+            provisionClient?.provision()
+            certName = provisionClient?.certName.toString()
+            keyName = provisionClient?.keyName.toString()
+        }
+    }
+
+    private fun getClientId(certPath: File?): String {
+        val idFile = context.getString(R.string.idFile)
+        var clientId = UUID.randomUUID().toString()
+        try {
+            clientId = File(certPath, idFile).readText()
+        } catch (e: FileNotFoundException) {
+            Log.e(MainActivity.TAG, "id file not found will proceed to create one")
+            File(certPath, idFile).printWriter().use { out ->
+                out.write(clientId)
+            }
+        }
+        return clientId
+    }
+
+    fun subscribe(topic: String, callback: (String) -> Unit) {
+        Log.d(TAG, "Subscribing to topic $topic")
+        client?.subscribe(topic, callback)
+    }
+
+    fun publish(topic: String, message: String) {
+        Log.d(TAG, "Publishing to $topic message: $message")
+        client?.publish(topic, message)
+    }
+
+    companion object {
+        const val TAG: String = "IoT"
+    }
+}
--- agent/android/Crescent/app/src/main/java/com/displaynote/crescent/MainActivity.kt
+++ agent/android/Crescent/app/src/main/java/com/displaynote/crescent/MainActivity.kt
@@ -11,76 +11,6 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var certPath : File
-    private lateinit var endpoint : String
-    private lateinit var provisioningTemplate : String
-
-    private lateinit var rootCert : String
-    private lateinit var certName : String
-    private lateinit var keyName : String
-    private lateinit var claimCert : String
-    private lateinit var claimKey : String
-    private var client : Client? = null
-
-    private fun loadCerts() {
-        rootCert = Util.readTextFileFromAssets(
-                applicationContext,
-                "AmazonRootCA1.pem").toString()
-
-        certPath.walkTopDown().filter { file ->
-            file.isFile && file.extension == "crt"
-        }.forEach { certName = it.name }
-
-        certPath.walkTopDown().filter { file ->
-            file.isFile && file.extension == "key"
-        }.forEach { keyName = it.name }
-
-        claimCert = Util.readTextFileFromAssets(
-                applicationContext,
-                getString(R.string.claimCert)
-        ).toString()
-
-        claimKey = Util.readTextFileFromAssets(
-                applicationContext,
-                getString(R.string.claimKey)
-        ).toString()
-    }
-
-    private fun provisionDevice() {
-        if (certName.isEmpty() || keyName.isEmpty()) {
-            val settings = ProvisionSettings(
-                    getClientId(certPath),
-                    claimCert,
-                    claimKey,
-                    rootCert,
-                    endpoint,
-                    provisioningTemplate,
-                    certPath
-            )
-            val provisionClient = ProvisioningClient(settings)
-            provisionClient.provision()
-            certName = provisionClient.certName.toString()
-            keyName = provisionClient.keyName.toString()
-        }
-    }
-
-    private fun initIot() {
-        Log.d(TAG, "Initialising IoT subsystem")
-        certPath = applicationContext.filesDir
-        endpoint = getString(R.string.endpoint)
-        provisioningTemplate = getString(R.string.provisioningTemplate)
-        loadCerts()
-        provisionDevice()
-        if (certName.isNotEmpty() && keyName.isNotEmpty()) {
-            val cert = File(certPath, certName).readText()
-            val key = File(certPath, keyName).readText()
-            val clientSettings = ClientSettings(getClientId(certPath), cert, key, rootCert, endpoint)
-            client = Client(clientSettings)
-            client?.connect()
-            client?.subscribe("topic", ::onSubscribeReceived)
-            Log.d(TAG, "Connected and subscribed to IoT")
-        }
-    }
     private fun onSubscribeReceived(payload: String) {
         Log.d(Companion.TAG, "payload received: $payload")
     }
@@ -90,26 +20,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btn = findViewById<Button>(R.id.start_button)
 
-        startJob(applicationContext)
-        initIot()
+        val certPath = applicationContext.filesDir
+        val endpoint = getString(R.string.endpoint)
+        val provisioningTemplate = getString(R.string.provisioningTemplate)
+
+        val iot = IoT(applicationContext, certPath, endpoint, provisioningTemplate)
+        iot.subscribe("topic", ::onSubscribeReceived)
 
         btn.setOnClickListener {
-            client?.publish("topic", "{\"message\":\"hello\"}")
+            iot.publish("topic", "{\"message\":\"hello\"}")
         }
-    }
 
-    private fun getClientId(certPath: File?): String {
-        val idFile = getString(R.string.idFile)
-        var clientId = UUID.randomUUID().toString()
-        try {
-            clientId = File(certPath, idFile).readText()
-        } catch (e: FileNotFoundException) {
-            Log.e(TAG, "id file not found will proceed to create one")
-            File(certPath, idFile).printWriter().use { out ->
-                out.write(clientId)
-            }
-        }
-        return clientId
+        startJob(applicationContext)
     }
 
     companion object {
