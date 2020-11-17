980b8406df801921b337506dc4cf9d0c17e7692e
Andy Bell <andy.bell@displaynote.com>
2020-11-17 16:47:48 +0100

    Return list of shadows

--- web/amplify/backend/function/func0crescent0list0lfd/src/index.js
+++ web/amplify/backend/function/func0crescent0list0lfd/src/index.js
@@ -3,15 +3,43 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
+const AWS = require('aws-sdk');
+const iotClient = new AWS.Iot();
+
+var region = process.env.REGION;
+
+AWS.config.update({
+    region: region
+});
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+
+    var resultArray = [];
+
+    var params = {
+        queryString: 'shadow.reported.name:* AND thingTypeName:LFD'
     };
-    return response;
+
+    try {
+
+        var result = await iotClient.searchIndex(params).promise();
+
+        //build an array of the thing shadow values and return array
+        result.things.forEach(element => {
+
+            var shadow = JSON.parse(element.shadow);
+
+            shadow.reported.lfdId = element.thingName;
+
+            resultArray.push(shadow.reported);
+        });
+
+        return resultArray;
+    }
+    catch (err) {
+
+        console.log("error: " + err);
+        throw err;
+    }
 };
+
