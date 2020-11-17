123210755a81a97dc933bd2d455c10e5efd76f49
Andy Bell <andy.bell@displaynote.com>
2020-11-17 16:45:54 +0100

    Add cloud formation files and others Add list shadow lambda with profile updated for iot:search

---
+++ web/src/graphql/subscriptions.js
@@ -0,0 +1,63 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateState = /* GraphQL */ `
+  subscription OnCreateState {
+    onCreateState {
+      id
+      lfdId
+      upTime
+      OPS
+      Front
+      HDMI1
+      HDMI2
+      HDMI3
+      DP
+      VGA
+      usb
+      brightness
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateState = /* GraphQL */ `
+  subscription OnUpdateState {
+    onUpdateState {
+      id
+      lfdId
+      upTime
+      OPS
+      Front
+      HDMI1
+      HDMI2
+      HDMI3
+      DP
+      VGA
+      usb
+      brightness
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteState = /* GraphQL */ `
+  subscription OnDeleteState {
+    onDeleteState {
+      id
+      lfdId
+      upTime
+      OPS
+      Front
+      HDMI1
+      HDMI2
+      HDMI3
+      DP
+      VGA
+      usb
+      brightness
+      createdAt
+      updatedAt
+    }
+  }
+`;
